239c45af4e88e14232c00f1f681a00f71f9e8ebd
TalvenlapsiDot <kukkajessika@gmail.com>
2022-09-06 02:31:24 +0300

    Social media buttons in theme!

--- components/Footer.js
+++ components/Footer.js
@@ -6,11 +6,11 @@ const Footer = () => {
     return (
         <div className={styles.footer}>
         <p>
-        <SocialIcon style={{ height: 35, width: 35 }} fgColor="#FFFFFF" url="https://twitter.com/talvenlapsiDot" />
-        <SocialIcon style={{ height: 35, width: 35 }} fgColor="#FFFFFF" url="https://www.facebook.com/J.Kukka" />
-        <SocialIcon style={{ height: 35, width: 35 }} fgColor="#FFFFFF" url="https://www.instagram.com/talvenlapsi/" />
-        <SocialIcon style={{ height: 35, width: 35 }} fgColor="#FFFFFF" url="https://ko-fi.com/talvenlapsi" />
-        <SocialIcon style={{ height: 35, width: 35 }} fgColor="#FFFFFF" url="https://www.twitch.tv/talvenlapsi" />
+        <SocialIcon style={{ height: 35, width: 35 }} fgColor="#FFFFFF" bgColor='#73414c' url="https://twitter.com/talvenlapsiDot" />
+        <SocialIcon style={{ height: 35, width: 35 }} fgColor="#FFFFFF" bgColor='#73414c' url="https://www.facebook.com/J.Kukka" />
+        <SocialIcon style={{ height: 35, width: 35 }} fgColor="#FFFFFF" bgColor='#73414c' url="https://www.instagram.com/talvenlapsi/" />
+        <SocialIcon style={{ height: 35, width: 35 }} fgColor="#FFFFFF" bgColor='#73414c' url="https://ko-fi.com/talvenlapsi" />
+        <SocialIcon style={{ height: 35, width: 35 }} fgColor="#FFFFFF" bgColor='#73414c' url="https://www.twitch.tv/talvenlapsi" />
         </p>
       </div>
     );
