7694c186d92f2b19c9760f8e71e525a980d15aa8
TalvenlapsiDot <kukkajessika@gmail.com>
2022-09-09 19:56:39 +0300

    Meh.

--- pages/gallery/index.js
+++ pages/gallery/index.js
@@ -1,23 +1,7 @@
 import Head from 'next/head';
 import Page from '../../components/Page';
-import React, { useState, useCallback} from 'react';
-import Gallery from "react-photo-gallery";
-import Carousel, { Modal, ModalGateway } from "react-images";
-import { photos } from "./photos";
 
 export default function GalleryPage() {
-  const [currentImage, setCurrentImage] = useState(0);
-  const [viewerIsOpen, setViewerIsOpen] = useState(false);
-
-  const openLightbox = useCallback((event, { photo, index }) => {
-    setCurrentImage(index);
-    setViewerIsOpen(true);
-  }, []);
-
-  const closeLightbox = () => {
-    setCurrentImage(0);
-    setViewerIsOpen(false);
-  };
 
     return (
       <Page>
@@ -29,21 +13,6 @@ export default function GalleryPage() {
           <h1> This is the gallery </h1>
           <p>Test text and shit.</p>
           <p>Will be pictures one day.</p>
-          <Gallery photos={photos} onClick={openLightbox} />
-      <ModalGateway>
-        {viewerIsOpen ? (
-          <Modal onClose={closeLightbox}>
-            <Carousel
-              currentIndex={currentImage}
-              views={photos.map(x => ({
-                ...x,
-                srcset: x.srcSet,
-                caption: x.title
-              }))}
-            />
-          </Modal>
-        ) : null}
-      </ModalGateway>
         </section>
     </Page>
     )
