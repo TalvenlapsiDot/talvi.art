81d5e0a39b73b262edb9707f05f5ac71a3d9932e
TalvenlapsiDot <kukkajessika@gmail.com>
2022-09-09 22:51:00 +0300

    Trying with Gallery and Failing

--- pages/gallery/index.js
+++ pages/gallery/index.js
@@ -1,19 +1,42 @@
 import Head from 'next/head';
+import Image from 'next/image';
 import Page from '../../components/Page';
+import { getAllGalleryFilenames } from '../../lib/images';
 
-export default function GalleryPage() {
+export async function getStaticProps({ params }) {
+  const images = getAllGalleryFilenames();
+  return {
+    props: {
+      images,
+    },
+  };
+}
 
+
+
+export default function Gallery({ images }) {
+ /* const ImageDisplay =  images.map(function(element) {
+    <Image src={images.file}/>
+    console.log(element)
+  }); */
+  const ImageDisplay = () => {
+    return (
+      images.map((images) => (
+        <Image src={images.file} layout="fill"/>
+      ))
+    )
+  }
     return (
       <Page>
         <Head>
             <title> Gallery</title>
         </Head>
-
         <section>
           <h1> This is the gallery </h1>
           <p>Test text and shit.</p>
-          <p>Will be pictures one day.</p>
+          <ImageDisplay />
         </section>
     </Page>
     )
 }
+
